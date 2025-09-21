0ef940ed4f51cadc320bcaa13c02b94de1954a95
daman599 <damanpreetk117@gmail.com>
2025-09-21 14:40:31 +0530

    few fixes

--- component/Buttons.tsx
+++ component/Buttons.tsx
@@ -9,13 +9,15 @@ export default function Buttons() {
       <div className="hidden md:flex gap-3 fixed md:right-6 lg:right-18 bottom-8 font-medium text-xs">
          <button onClick={() => { setBtn("Personal") }}
             className={`${btn != "Personal" && "bg-black text-white"} 
-            cursor-pointer rounded-2xl py-1 px-4 transition-all duration-300 ease-in-out`}>
+            ${btn == "Personal" ? "cursor-not-allowed" : "cursor-pointer"} rounded-2xl py-1 px-4 transition-all duration-300 ease-in-out`}
+         >
             Personal
          </button>
 
          <button onClick={() => { setBtn("Business") }}
             className={`${btn != "Business" && "bg-black text-white"} 
-            rounded-2xl py-1 px-4 cursor-pointer transition-all duration-300 ease-in-out`}>
+            ${btn == "Business" ? "cursor-not-allowed" : "cursor-pointer"} rounded-2xl py-1 px-4 transition-all duration-300 ease-in-out`}
+         >
             Business
          </button>
       </div>
