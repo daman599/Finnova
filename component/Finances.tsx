fd864053d9df1a359b0ff5c3bb3ff2d842931d00
daman599 <damanpreetk117@gmail.com>
2025-09-23 11:49:59 +0530

    added few more divs

--- component/Finances.tsx
+++ component/Finances.tsx
@@ -4,12 +4,7 @@ export default function Finances() {
   return (
     <div className="relative flex flex-col items-center mt-20">
 
-      <div className="flex items-center justify-center
-                      font-medium text-sm text-[#058E00] 
-                      p-1 rounded-full
-                      backdrop-blur-md bg-white
-                      shadow-lg shadow-[#bebebe]/40
-                      mb-6 w-fit">
+      <div className="flex items-center justify-center font-medium text-sm text-[#058E00] p-1 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#bebebe]/40 mb-6 w-fit">
         <Dot size={30} className="animate-pulse" />
         <p className="pr-3">Daily Finances</p>
       </div>
@@ -22,19 +17,10 @@ export default function Finances() {
         Empowering global financial management.
       </p>
 
-      <div>
-
-      </div>
-
       <div className="flex">
         <div className="absolute left-20 top-96 flex-col max-w-xl px-6 md:px-12 lg:px-20 py-20">
 
-          <div className="flex items-center justify-center
-                      font-medium text-sm text-[#058E00] 
-                      p-1 rounded-full
-                      backdrop-blur-md bg-white
-                      shadow-lg shadow-[#058E00]/40
-                      mb-6 w-fit">
+          <div className="flex items-center justify-center font-medium text-sm text-[#058E00] p-1 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#058E00]/40 mb-6 w-fit">
             <Dot size={30} className="animate-pulse" />
             <p className="pr-3">Account Transfer</p>
           </div>
@@ -48,11 +34,11 @@ export default function Finances() {
           </p>
         </div>
 
-        <div className="bg-[#F6F6F6] rounded-4xl rounded-b-none h-[700px] w-[510px] mb-3 ml-[330px] mt-[85px] px-[56px] py-[74px]">
+        <div className="bg-[#F6F6F6] rounded-4xl  h-[680px] w-[510px] mb-3 ml-[330px] mt-[85px] px-[56px] py-[74px]">
 
           <div className="bg-white rounded-3xl w-[400px] h-[520px] pl-9 ">
             <p className="font-medium text-xl pt-20">Amount</p>
-            <p className="font-medium text-xs pt-4 text-[#9F9F9F] ">Send Money</p>
+            <p className="font-medium text-xs pt-7 text-[#9F9F9F] ">Send Money</p>
 
             <div className="bg-[#F6F6F6] max-w-[325px] max-h-[65px] rounded-2xl mt-2 flex gap-24">
 
@@ -69,12 +55,29 @@ export default function Finances() {
 
             </div>
 
+            <p className="font-medium text-xs pt-10 text-[#9F9F9F] ">Who are you sending your money to?</p>
+
+            <div className="bg-[#F6F6F6] max-w-[325px] max-h-[65px] rounded-2xl mt-2 flex gap-40">
+
+              <div className="p-3 flex items-center justify-center gap-3">
+                <div className="w-9 h-9 rounded-full bg-[#c31574] flex justify-center text-base text-white items-center">J</div>
+                <p>John</p>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <ChevronDown size={30} className="text-[#A0A0A0]" />
+              </div>
+            </div>
+
+            <div className="bg-black h-[45px] w-[260px] rounded-3xl flex items-center justify-center mt-16 mx-8 cursor-pointer">
+              <p className="font-normal text-base text-[#9F9F9F]">Confirm and Send</p>
+            </div>
 
           </div>
+
         </div>
 
       </div>
-
     </div>
   );
 }
