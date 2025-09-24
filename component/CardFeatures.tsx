b229b6c24a78b7b2f0e009b77b5d4730cd1fb4dd
daman599 <damanpreetk117@gmail.com>
2025-09-24 21:51:56 +0530

    glowy border added

--- component/CardFeatures.tsx
+++ component/CardFeatures.tsx
@@ -19,8 +19,8 @@ export default function CardFeatures() {
     <div className="bg-black w-[1300px] h-[756px] rounded-4xl my-30 mx-28 flex flex-col ">
 
       <div className="flex flex-col justify-center items-center gap-3">
-        <div className="bg-[#262424] mt-14 w-fit text-[#395828] rounded-t-[#FFFFFF] font-medium text-sm flex items-center justify-center py-2 px-3 rounded-xl">
-          <Dot size={30} className="animate-ping" />
+        <div className="bg-[#262424] mt-14 border-t-1 border-opacity-20 border-[#9F9F9F] w-fit text-[#22C55E] rounded-t-[#FFFFFF] font-medium text-sm flex items-center justify-center py-2 px-3 rounded-xl">
+          <Dot size={30} className="animate-pulse" />
           <p className="pr-3">Card Features</p>
         </div>
 
@@ -28,13 +28,13 @@ export default function CardFeatures() {
         <p className="pt-6 text-normal text-base text-[#9F9F9F]">Discover the functions that make your card more than just payment.</p>
       </div>
 
-      <div className="mt-16 ml-28 flex gap-4 ">
+      <div className="mt-16 ml-28 flex gap-5">
         {Icons.map((obj, i) => {
           const Icon = obj.icon;
 
           return (
             <div key={i}
-              className="w-[345px] h-[286px] flex flex-col px-8 py-6 bg-[#1E1E1E] rounded-4xl ">
+              className="w-[345px] h-[286px] flex flex-col border-l-2 border-t-2 border-[#395828] shadow-[0_0_12px_#395828] px-8 py-6 bg-[#1E1E1E] rounded-4xl ">
 
               <Icon size={48} color={obj.color} />
               <p className="py-3 text-medium text-2xl text-white">{obj.label}</p>
