7886727edf9b02419490092c1552d04c59baa7f2
daman599 <damanpreetk117@gmail.com>
2025-09-27 17:11:43 +0530

    personal block added

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,42 +1,94 @@
+"use client";
+
+import { useState } from "react";
 import { inriaSerif } from "@/lib/font";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ArrowRight } from "lucide-react";
+
+interface CardType {
+  title: string;
+  features: string[];
+}
+
+const Card: CardType[] = [
+  { title: "Daily Finances", features: ["Account Transfer", "Analytics", "Budgeting", "Accounts "] },
+  { title: "Finnova Card", features: ["Card", "Card features"] },
+  { title: "Investments", features: ["Global Transfers ", "Security", "Stocks"] },
+  { title: "Crypto", features: ["Various crypto assests ", "Advantages"] },
+  { title: "Prices", features: ["Standard ", "Pro", "Ultra pro max"] },
+];
 
 export default function Navbar() {
+  const [active, setActive] = useState<boolean | null>(false);
+
   return (
-    <nav className="w-full fixed top-0 left-0 z-50 bg-white/30 backdrop-blur-md text-black px-1 md:px-6 lg:px-13 py-4 md:py-2 flex items-center justify-between">
-      <div className="hidden md:flex items-center gap-1 lg:gap-3">
-        {["Personal", "Business", "Company"].map((item, index) => (
-          <button
-            key={index}
-            className="px-1 md:px-3 py-0.5 md:py-1 rounded-xl hover:bg-gray-200 font-medium text-xs text-gray-600 flex items-center gap-0.5 md:gap-1"
-          >
-            {item}
-            <ChevronDown size={14} className="text-gray-400" />
-          </button>
-        ))}
-      </div>
-
-      <div
-        className={`absolute left-1/2 transform -translate-x-1/2 text-2xl md:text-3xl lg:text-4xl font-bold ${inriaSerif.className}`}
-      >
-        Finnova
-      </div>
-
-      <div className="hidden md:flex items-center gap-4 md:gap-6">
-        {["Help", "Blog", "En"].map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-0.5 md:gap-1 font-semibold text-xs text-gray-700 cursor-pointer"
-          >
-            {item}
-            {index === 2 && <ChevronDown size={12} className="text-gray-600" />}
-          </div>
-        ))}
-
-        <div className="px-2 md:px-4 py-0.5 md:py-1 rounded-full shadow-md shadow-[#058E00]/20 bg-black text-white text-xs font-medium cursor-pointer hover:scale-105 transition">
-          Log in
+    <>
+      <nav className="w-full fixed top-0 left-0 z-50 bg-white/30 backdrop-blur-md text-black px-1 md:px-6 lg:px-13 py-4 md:py-2 flex items-center justify-between">
+        <div className="hidden md:flex items-center gap-1 lg:gap-3">
+          {["Personal", "Business", "Company"].map((item, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                setActive(item == "Personal" ? !active : null);
+              }}
+              className="px-1 md:px-3 py-0.5 md:py-1 rounded-xl hover:bg-gray-200 font-medium text-xs text-gray-600 flex items-center gap-0.5 md:gap-1"
+            >
+              {item}
+              <ChevronDown size={14} className="text-gray-400" />
+            </button>
+          ))}
         </div>
-      </div>
-    </nav>
+
+        <div
+          className={`absolute left-1/2 transform -translate-x-1/2 text-2xl md:text-3xl lg:text-4xl font-bold ${inriaSerif.className}`}
+        >
+          Finnova
+        </div>
+
+        <div className="hidden md:flex items-center gap-4 md:gap-6">
+          {["Help", "Blog", "En"].map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-0.5 md:gap-1 font-semibold text-xs text-gray-700 cursor-pointer"
+            >
+              {item}
+              {index === 2 && <ChevronDown size={12} className="text-gray-600" />}
+            </div>
+          ))}
+
+          <div className="px-2 md:px-4 py-0.5 md:py-1 rounded-full shadow-md shadow-[#058E00]/20 bg-black text-white text-xs font-medium cursor-pointer hover:scale-105 transition">
+            Log in
+          </div>
+        </div>
+      </nav >
+
+      {/* Personal Block */}
+      {active && (
+        <div className="absolute top-20 left-14 w-[610px] h-[689px] rounded-4xl bg-[#eeeeee] mb-16 z-40 shadow-lg
+                  before:content-[''] before:absolute before:-top-3 before:left-6 
+                  before:border-l-8 before:border-r-8 before:border-b-8 
+                  before:border-l-transparent before:border-r-transparent before:border-b-[#eeeeee] ">
+          <div className="font-medium text-xl flex items-center px-7 py-6 gap-2">
+            <p>Discover Finnova Personal</p>
+            <ArrowRight />
+          </div>
+
+          <div className="flex flex-wrap px-1 py-5">
+            {Card.map((obj, index) => (
+              <div key={index} className="flex px-7 py-5 w-[45%] min-w-[300px]">
+                <div className="flex flex-col gap-3">
+                  <p className="font-semibold text-base">{obj.title}</p>
+                  <div className="flex flex-col space-y-2 font-medium text-base text-[#535353]">
+                    {obj.features.map((ele, i) => (
+                      <p key={i}>{ele}</p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )
+      }
+    </>
   );
 }
