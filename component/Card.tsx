0a92de2915056d044ff583bbcb96d42df81984ac
daman599 <damanpreetk117@gmail.com>
2025-10-05 00:33:17 +0530

    made card component responsive

--- component/Card.tsx
+++ component/Card.tsx
@@ -1,42 +1,46 @@
-import { Dot } from "lucide-react";
 import Image from "next/image";
 
 export default function Card() {
     return (
-        <div className="flex items-center justify-between px-6 md:px-12 lg:px-20 py-20">
-            <div className="flex flex-col max-w-xl pl-24">
-                <div className="flex items-center font-medium text-sm text-[#058E00] p-1 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#058E00]/40 mb-6 w-fit">
-                    <Dot size={30} className="animate-ping" />
-                    <p className="pr-3">Finnova Card</p>
+        <div className="flex flex-col md:flex-row items-center justify-center px-2 py-20 gap-4 ">
+
+            <div className="flex flex-col space-y-4 w-full md:max-w-xl items-start justify-center ">
+                <div className="flex items-center justify-center gap-3 px-5 py-2 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#058E00]/40 w-fit">
+                    <span className="w-1 h-1 rounded-full bg-[#058E00] ring-1 ring-[#2dd327] animate-ping
+                    shadow-background "></span>
+                    <span className="font-medium text-xs md:text-sm text-[#058E00]">Finnova Card</span>
                 </div>
 
-                <p className="text-start leading-13 font-semibold text-2xl sm:text-3xl md:text-5xl">
-                    Enhancing Your Financial Experience
-                </p>
+                <div className="flex flex-col items-start justify-center space-y-3 md:space-y-5">
+                    <p className="lg:leading-13 font-semibold text-2xl sm:text-3xl lg:text-5xl">
+                        Enhancing Your Financial Experience
+                    </p>
 
-                <p className="text-[#9F9F9F] max-w-96 font-medium text-xs sm:text-sm md:text-base mt-4">
-                    Discover the range of benefits and features that make our card stand out.
-                </p>
+                    <p className="text-[#9F9F9F] w-full sm:max-w-96 font-medium text-xs sm:text-sm md:text-base ">
+                        Discover the range of benefits and features that make our card stand out.
+                    </p>
 
-                <div className="bg-black w-fit px-3 py-1 rounded-2xl mt-3">
-                    <p className="font-medium text-xs text-[#9F9F9F]">Get my card</p>
+                    <div className="bg-black w-fit px-3 py-1 flex items-center justify-center rounded-2xl ">
+                        <span className="font-medium text-xs text-[#9F9F9F]">Get my card</span>
+                    </div>
                 </div>
             </div>
 
-            <div className="flex-shrink-0 pr-28 py-8 relative">
+            <div className="relative py-2 ">
                 <Image
-                    src={"/hand.svg"}
+                    src="/hand.svg"
                     alt="hand holding card"
                     width={450}
                     height={450}
+                    className="w-72 sm:w-80 md:w-[26rem] lg:w-[30rem] h-full"
                 />
 
                 <Image
-                    src={"/card.svg"}
+                    src="/card.svg"
                     alt="card"
                     width={210}
                     height={210}
-                    className="absolute top-43 left-22 "
+                    className="absolute w-24 md:w-27 lg:w-36 top-28 left-20 md:top-23 md:left-14 lg:top-31 lg:left-20 h-auto"
                 />
             </div>
         </div>
