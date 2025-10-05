165de11c0a4c561a223ee498275b4e3d2b6fd4c4
daman599 <damanpreetk117@gmail.com>
2025-10-05 15:54:11 +0530

    made optimizations to component

--- component/Investment.tsx
+++ component/Investment.tsx
@@ -1,35 +1,67 @@
-import { Dot } from "lucide-react";
 import Image from "next/image";
-import { US, EU, JP } from "country-flag-icons/react/3x2";
+import { US, EU, JP, ZA } from "country-flag-icons/react/3x2";
+
+interface CardType {
+  title: string;
+  extra: string;
+  children: React.ReactNode
+}
+
+function Card({ title, extra, children }: CardType) {
+  return (
+    <div className="bg-[#F6F6F6] flex flex-col space-y-2 sm:space-y-4 aspect-[3/4] w-[451px] h-[644px] rounded-4xl px-6 py-7">
+
+      <p className="font-semibold sm:text-xl md:text-2xl">{title}</p>
+      <p className="text-[#A0A0A0] font-medium sm:text-sm md:text-base">Experience the freedom of hassle free modern transactions and say
+        goodbye to unnecessary fears and enjoy seamless transactions.</p>
+
+      <div className="bg-black w-fit rounded-full px-3 py-2 flex items-center justify-center">
+        <span className="font-medium text-xs text-[#9F9F9F]">{extra}</span>
+      </div>
+
+      {children}
+    </div>
+  );
+}
+interface countryCardType {
+  countryFlag: React.ComponentType<{ className: string }>;
+  currency: string;
+  currencyName: string;
+  budget: string;
+  z: string;
+}
+
+const CountryCards: countryCardType[] = [
+  { countryFlag: JP, currency: "JPY", currencyName: "Japanese Yen", budget: "+¥1500.00", z: "30" },
+  { countryFlag: US, currency: "USD", currencyName: "US Dollar", budget: "+$290.99", z: "20" },
+  { countryFlag: EU, currency: "EUR", currencyName: "Euro", budget: "-€20.99", z: "10" },
+];
 
 export default function Investment() {
   return (
-    <div className="relative flex flex-col items-center mt-20">
+    <section className="flex flex-col items-center space-y-20 py-16 sm:py-20 px-2 md:px-8 lg:px-12 overflow-hidden">
 
-      <div className="flex items-center justify-center font-medium text-sm text-[#058E00] p-1 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#bebebe]/40 mb-6 w-fit">
-        <Dot size={30} className="animate-pulse" />
-        <p className="pr-3">Investments</p>
+      {/* Header Section */}
+      <div className="flex flex-col items-center text-center space-y-3 sm:space-y-4">
+        <div className="flex items-center justify-center gap-2 sm:gap-3 px-3 sm:px-4 py-1.5 sm:py-2 rounded-full backdrop-blur-md bg-white shadow-lg shadow-[#bebebe]/40">
+          <div className="w-1 h-1 rounded-full bg-[#058E00] ring-1 ring-[#2dd327] animate-ping"></div>
+          <span className="font-medium text-xs sm:text-sm text-[#058E00]">Investments</span>
+        </div>
+
+        <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl max-w-2xl sm:max-w-3xl text-center">
+          Spend, save and manage your money, all in one place.
+        </h2>
+
+        <p className="font-medium text-[#A0A0A0] text-xs sm:text-sm md:text-base">
+          Open a full bank account from your phone, for free.
+        </p>
       </div>
 
-      <p className="font-semibold text-5xl text-center px-[355px]">
-        Spend, save and manage your money, all in one place.
-      </p>
-
-      <p className="font-medium text-base text-center mt-5 text-[#A0A0A0]">
-        Open a full bank account from your phone, for free.
-      </p>
-
-
-      <div className="my-14 flex gap-12">
-        <div className="bg-[#F6F6F6] w-[451px] h-[636px] rounded-4xl px-6 py-7">
-          <p className="font-semibold text-2xl">Security that never sleeps</p>
-          <p className="text-[#A0A0A0] font-medium text-base py-3 pr-3">Experience the freedom of hassle free modern transactions and say
-            goodbye to unnecessary fears and enjoy seamless transactions.</p>
-
-          <div className="bg-black w-fit rounded-2xl px-3 py-1 my-2">
-            <p className="font-medium text-xs text-[#9F9F9F]">Explore security</p>
-          </div>
-
+      <div className="flex flex-col sm:flex-row gap-12">
+        <Card
+          title={"Security that never sleeps"}
+          extra={"Explore security"}
+        >
           <div className="relative">
             <Image
               src={"/black-card.svg"}
@@ -46,80 +78,49 @@ export default function Investment() {
               alt={"card"}
               className="absolute top-32 left-40 "
             />
-
           </div>
+        </Card>
 
-        </div>
+        <Card
+          title={"Free global transfers"}
+          extra={"Explore transfers"}
+        >
+          <div className="relative flex flex-col items-center justify-center mt-10">
+            {CountryCards.map((card, i) => {
+              const Flag = card.countryFlag;
 
-        <div className="bg-[#F6F6F6] w-[451px] h-[636px] rounded-4xl px-6 py-7">
-          <p className="font-semibold text-2xl">Free global transfers</p>
-          <p className="text-[#A0A0A0] font-medium text-base py-3 pr-3">Experience the freedom of hassle free modern transactions and say
-            goodbye to unnecessary fears and enjoy seamless transactions.</p>
-
-          <div className="bg-black w-fit rounded-2xl px-3 py-1 my-2">
-            <p className="font-medium text-xs text-[#9F9F9F]">Explore transfers</p>
-          </div>
-
-          <div className="relative flex flex-col items-center mt-8">
-            {/* Japan Card */}
-            <div className="bg-white rounded-4xl flex px-6 py-2 shadow-lg shadow-[#A0A0A0] w-fit h-fit relative z-20 ">
-              <JP className="w-9 h-9 " />
-              <div className="flex justify-between gap-24">
-                <div className="flex flex-col px-4">
-                  <p className="font-medium text-base">Japanese Yen</p>
-                  <p className="font-normal text-xs text-[#B5B5B5]">JPY</p>
+              return (
+                <div
+                  key={i}
+                  className={`bg-white rounded-full flex px-6 py-2 shadow-lg shadow-[#A0A0A0] w-fit h-fit absolute transition-transform duration-300 hover:translate-y-2`}
+                  style={{ top: `${i * 40}px`, zIndex: card.z }}
+                >
+                  <Flag className="w-9 h-9" />
+                  <div className="flex justify-between gap-24">
+                    <div className="flex flex-col px-4">
+                      <p className="font-medium text-base">{card.currencyName}</p>
+                      <p className="font-normal text-xs text-[#B5B5B5]">{card.currency}</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <span className="font-medium text-xs">{card.budget}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex items-center justify-center">
-                  <p className="font-medium text-xs">+¥1500.00</p>
-                </div>
-              </div>
-            </div>
+              );
+            })}
 
-            {/* USD Card */}
-            <div className="bg-white rounded-4xl flex px-6 py-2 shadow-lg shadow-[#A0A0A0] w-fit h-fit -mt-3 relative z-10">
-              <US className="w-9 h-9 " />
-              <div className="flex justify-between gap-24">
-                <div className="flex flex-col px-4">
-                  <p className="font-medium text-base">US Dollar</p>
-                  <p className="font-normal text-xs text-[#B5B5B5]">USD</p>
-                </div>
-                <div className="flex items-center justify-center">
-                  <p className="font-medium text-xs">+$290.99</p>
-                </div>
-              </div>
-            </div>
-
-            {/* Euro Card */}
-            <div className="bg-white rounded-4xl flex px-6 py-2 shadow-lg shadow-[#A0A0A0] w-fit h-fit -mt-5 relative z-5">
-              <EU className="w-9 h-9 " />
-              <div className="flex justify-between gap-24">
-                <div className="flex flex-col px-4">
-                  <p className="font-medium text-base">Euro</p>
-                  <p className="font-normal text-xs text-[#B5B5B5]">EUR</p>
-                </div>
-                <div className="flex items-center justify-center">
-                  <p className="font-medium text-xs">-€20.99</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
-          <div className="relative">
             <Image
               src={"/earth.svg"}
               width={500}
               height={100}
               alt={"earth"}
-              className="absolute top-22"
+              className="absolute top-50"
             />
 
           </div>
-        </div>
+        </Card>
+      </div >
 
-      </div>
-
-    </div>
+    </section >
   );
 }
